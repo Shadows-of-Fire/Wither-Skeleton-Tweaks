405e121ea8fe526db6beff28f06af7a20652363a
Shadows_of_Fire <Bward7864@gmail.com>
2018-01-04 20:48:06 -0500

    bump ver

--- src/main/java/shadows/wstweaks/WSTweaks.java
+++ src/main/java/shadows/wstweaks/WSTweaks.java
@@ -22,7 +22,7 @@ import shadows.wstweaks.util.Events;
 public class WSTweaks {
 	public static final String MODID = "witherskelefix";
 	public static final String MODNAME = "Wither Skeleton Tweaks";
-	public static final String VERSION = "2.3.0";
+	public static final String VERSION = "2.3.1";
 
 	@SidedProxy(clientSide = "shadows.wstweaks.proxy.ClientProxy", serverSide = "shadows.wstweaks.proxy.CommonProxy")
 	public static CommonProxy proxy;
