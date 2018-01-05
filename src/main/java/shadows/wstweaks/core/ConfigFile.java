033b4fa1b656d5b9a14a06bf1495e45642767f30
Shadows_of_Fire <Bward7864@gmail.com>
2018-01-04 20:21:31 -0500

    fix the config

--- src/main/java/shadows/wstweaks/core/ConfigFile.java
+++ src/main/java/shadows/wstweaks/core/ConfigFile.java
@@ -18,15 +18,15 @@ public class ConfigFile {
 
 		config.load();
 
-		shardValue = config.getInt(Configuration.CATEGORY_GENERAL, "Shard Value", 9, 1, 9, "The value of a skull fragment, in terms of 1/n (How many shards per skull) Valid values are 1-9");
-		enableLava = config.getBoolean(Configuration.CATEGORY_GENERAL, "Enable Lava Immolation Blade", true, "If the Immolation Blade (Lava) is enabled");
-		enableBlaze = config.getBoolean(Configuration.CATEGORY_GENERAL, "Enable Blaze Immolation Blade", true, "If the Immolation Blade (Blaze) is enabled");
-		shardDropChance = config.getInt(Configuration.CATEGORY_GENERAL, "Fragment Drop Chance", 1, 0, Integer.MAX_VALUE, "The wither skull fragment drop chance in 1/n.  Higher numbers means less drops. Set to 0 to disable.");
-		extraWitherSkeletons = config.getInt(Configuration.CATEGORY_GENERAL, "Extra Wither Skeletons", 0, 0, 10, "How many extra wither skeletons are spawned when one would be.");
-		extraSpawns = config.getBoolean(Configuration.CATEGORY_GENERAL, "Extra Spawns", false, "If, when a blaze or pigman spawns on Nether Brick, a wither skeleton spawns with it.");
-		allowAllBiomes = config.getBoolean(Configuration.CATEGORY_GENERAL, "All Biomes", false, "If ALL skeletons in any Biome are turned into Wither Skeletons.");
-		allBiomesChance = config.getInt(Configuration.CATEGORY_GENERAL, "All Biomes Chance", 1, 1, Integer.MAX_VALUE, "The 1/n chance for (if \"All Biomes\" is true) a skeleton in a non-Hell biome to be a Wither Skeleton");
-		delSwords = config.getBoolean(Configuration.CATEGORY_GENERAL, "Delete Stone Swords", true, "Whether or not to remove stone swords from wither skeleton drops.");
+		shardValue = config.getInt("Shard Value", Configuration.CATEGORY_GENERAL, 9, 1, 9, "The value of a skull fragment, in terms of 1/n (How many shards per skull) Valid values are 1-9");
+		enableLava = config.getBoolean("Enable Lava Immolation Blade", Configuration.CATEGORY_GENERAL, true, "If the Immolation Blade (Lava) is enabled");
+		enableBlaze = config.getBoolean("Enable Blaze Immolation Blade", Configuration.CATEGORY_GENERAL, true, "If the Immolation Blade (Blaze) is enabled");
+		shardDropChance = config.getInt("Fragment Drop Chance", Configuration.CATEGORY_GENERAL, 1, 0, Integer.MAX_VALUE, "The wither skull fragment drop chance in 1/n.  Higher numbers means less drops. Set to 0 to disable.");
+		extraWitherSkeletons = config.getInt("Extra Wither Skeletons", Configuration.CATEGORY_GENERAL, 0, 0, 10, "How many extra wither skeletons are spawned when one would be.");
+		extraSpawns = config.getBoolean("Extra Spawns", Configuration.CATEGORY_GENERAL, false, "If, when a blaze or pigman spawns on Nether Brick, a wither skeleton spawns with it.");
+		allowAllBiomes = config.getBoolean("All Biomes", Configuration.CATEGORY_GENERAL, false, "If ALL skeletons in any Biome are turned into Wither Skeletons.");
+		allBiomesChance = config.getInt("All Biomes Chance", Configuration.CATEGORY_GENERAL, 1, 1, Integer.MAX_VALUE, "The 1/n chance for (if \"All Biomes\" is true) a skeleton in a non-Hell biome to be a Wither Skeleton");
+		delSwords = config.getBoolean("Delete Stone Swords", Configuration.CATEGORY_GENERAL, true, "Whether or not to remove stone swords from wither skeleton drops.");
 
 		if (config.hasChanged()) config.save();
 
