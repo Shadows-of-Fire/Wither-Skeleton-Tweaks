5bcf6c2c12da0b27b89bff920794a082b590e95d
Shadows_of_Fire <Bward7864@gmail.com>
2019-01-17 00:30:53 -0500

    Closes #12

--- src/main/java/shadows/wstweaks/WitherSkeletonTweaks.java
+++ src/main/java/shadows/wstweaks/WitherSkeletonTweaks.java
@@ -21,7 +21,7 @@ public class WitherSkeletonTweaks {
 
 	public static final String MODID = "witherskelefix";
 	public static final String MODNAME = "Wither Skeleton Tweaks";
-	public static final String VERSION = "2.6.2";
+	public static final String VERSION = "2.6.3";
 
 	@SidedProxy(clientSide = "shadows.wstweaks.proxy.ClientProxy", serverSide = "shadows.wstweaks.proxy.CommonProxy")
 	public static CommonProxy PROXY;
